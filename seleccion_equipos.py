f97edb45300241db0138413a1ded4070bd959565
José Ángel <al_jmartosa@iessierradelasvillas.org>
2023-01-13 11:11:09 +0100

    Hecha estructura de la imagen y descargadas

--- seleccion_equipos.py
+++ seleccion_equipos.py
@@ -2,13 +2,18 @@ import flet as ft
 
 
 def main(page:ft.Page):
-    page.title("Trabajo selección de equipo")
+    page.title="Trabajo selección de equipo"
+
+    img = ft.Image(
+        src=f"Real Madrid.png",
+        width=100,
+        height=100,
+        fit=ft.ImageFit.CONTAIN,
+    )
+    page.add(img)
 
 
 
 
 
-
-
-
-ft.app(target=main)
+ft.app(target=main, assets_dir="Imagenes")

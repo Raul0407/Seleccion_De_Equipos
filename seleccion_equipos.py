a00011201f631d3c62f6f7768d864cc624853dee
José Ángel <al_jmartosa@iessierradelasvillas.org>
2023-01-17 14:34:59 +0100

    Funcionalidad terminada

--- seleccion_equipos.py
+++ seleccion_equipos.py
@@ -3,23 +3,43 @@ import flet as ft
 def main(page: ft.Page):
     page.title="Trabajo selección de equipo"
 
+    def imagen(e):
+        if menu.value=="Liverpool":
+        
+            img.src="Liverpool.png"
+    
+        elif menu.value=="Real Madrid":
+        
+            img.src="Real Madrid.png"
+        elif menu.value=="Manchester City":
+        
+            img.src="Manchester City.png"
+        elif menu.value=="Bayern de Múnich":
+        
+            img.src="Bayern de munich.png"
+        else:
+            menu.value=="Paris Saint Germain"
+        
+            img.src="Paris Saint Germain.jpeg"
+        page.update()
+
     menu =  ft.Dropdown(label="Equipos",hint_text="Elige tu equipo",options=[
             ft.dropdown.Option("Real Madrid"),
             ft.dropdown.Option("Manchester City"),
             ft.dropdown.Option("Liverpool"),
             ft.dropdown.Option("Bayern de Múnich"),
             ft.dropdown.Option("Paris Saint Germain"),
-            ], autofocus=True)
-
+            ], autofocus=True, on_change=imagen)
+    
     img = ft.Image(
-        src=f"Real Madrid.png",
-        width=100,
-        height=100,
+        src=f"d",
+        width=300,
+        height=300,
         fit=ft.ImageFit.CONTAIN,
     )
     
-    page.add(img)
-    page.add(menu)
+    page.add(menu,img)
+
 
 
 
